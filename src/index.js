18aa33d9379c26b79e101bf8c486839c674f47dd
reign <the.reignn@gmail.com>
2018-12-05 12:14:12 +0200

    migration

--- src/index.js
+++ src/index.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Loading from './Components/Loading';
 import store from './Redux/Store';
 import './Css/index.css';
-import App from './Components/App';
-import About from './Components/About';
-import NotFound from './Components/NotFound';
+
+const LazyApp = lazy(() => import('./Components/App'));
+const LazyAbout = lazy(() => import('./Components/About'));
+const LazyNotFound = lazy(() => import('./Components/NotFound'));
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <main>
         <Switch>
-          <Route exact path="/" component={App} />
-          <Route exact path="/about" component={About} />
-          <Route component={NotFound} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <Suspense fallback={<Loading />}>
+                <LazyApp />
+              </Suspense>
+            )}
+          />
+          <Route
+            exact
+            path="/about"
+            render={() => (
+              <Suspense fallback={<Loading />}>
+                <LazyAbout />
+              </Suspense>
+            )}
+          />
+          <Route
+            render={() => (
+              <Suspense fallback={<Loading />}>
+                <LazyNotFound />
+              </Suspense>
+            )}
+          />
         </Switch>
       </main>
     </Router>
